efb699823ce6f872b30c38eb9fca3688adc6c93d
megabentilad <megabentilad@Moge-PC>
2019-11-27 19:18:59 +0100

    Resuelto de forma chusquera el cronómetro analógico

--- UD5 DOM/2reloj/JavaScript/script.js
+++ UD5 DOM/2reloj/JavaScript/script.js
@@ -1,9 +1,11 @@
 function accion(){
     var digital = document.getElementById("digital");
     var grados = document.getElementById("grados");
-    var aguja = document.getElementById("aguja");
+    var aguja = document.getElementById("marco");
     if(this.innerHTML === "Empezar"){
         var centisegundos = 0; 
+        left = 5/9/2;
+        top = 0;
         cronometro = setInterval(function(){
             if(parseInt(centisegundos/100/60%60) < 10){
                 digital.innerHTML = parseInt(centisegundos/100/3600) + " : 0";
@@ -25,26 +27,25 @@ function accion(){
             grados.innerHTML = "grados: " + parseInt(centisegundos*0.06%360);
             
             //0.27777777
-            var left = 5/9/2;
-            var top = 0;
+            
            // 25/90/18
-            if(centisegundos*0.06%360 < 90){
-                top += (5/9/2);
-            }
-            if(centisegundos*0.06%360 >= 90 && centisegundos*0.06%360 < 180){
-                left -= (5/9/2);
-            }
-            if(centisegundos*0.06%360 >= 180 && centisegundos*0.06%360 < 270){
-                top -= (5/9/2);
-            }
-            if(centisegundos*0.06%360 >= 270){
-                left += (5/9/2);
-            }
+//            if(centisegundos*0.06%360 < 90){
+//                top += (5/9/2/3000);
+//            }
+//            if(centisegundos*0.06%360 >= 90 && centisegundos*0.06%360 < 180){
+//                left -= (5/9/2/3000);
+//            }
+//            if(centisegundos*0.06%360 >= 180 && centisegundos*0.06%360 < 270){
+//                top -= (5/9/2/3000);
+//            }
+//            if(centisegundos*0.06%360 >= 270){
+//                left += (5/9/2/3000);
+//            }
 
             
             aguja.style.transform = "rotateZ(" + centisegundos*0.06%360 + "deg)";
-            aguja.style.left = centisegundos*0.06%360*left+50 + "%";
-            aguja.style.top = centisegundos*0.06%360*top + "%";
+//            aguja.style.left = centisegundos*0.06%360*left+50 + "%";
+//            aguja.style.top = centisegundos*0.06%360*top + "%";
             
             centisegundos++;
         },10); //milisegundos
